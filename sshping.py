7b402fa7d2b536c281b2cad2c76aa70f1c94c69f
Kamil Strzempowicz <kamil.strzempowicz@synaptics.com>
2020-12-03 17:33:56 +0100

    Fix pycharm warnings

--- sshping.py
+++ sshping.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 # Author: Arturo 'Buanzo' Busleiman - October 2020
-import sys
 import argparse
-from sshconf import read_ssh_config
+import sys
 from os.path import expanduser
+
 from pythonping import ping
-from pprint import pprint
+from sshconf import read_ssh_config
 
 __version__ = '0.1.2'
 
-class SSHPing():
-    def __init__(self, target=None, count=None, verbose=False, timeout=10):
-        if target is None:
-            return(ValueError('No target argument provided'))
-        else:
-            c = read_ssh_config(expanduser("~/.ssh/config"))
-            try:
-                self.target = c.host(target)['hostname']
-            except KeyError:
-                self.target = target
+
+class SSHPing:
+    def __init__(self, target, count=None, verbose=False, timeout=10):
+        config = read_ssh_config(expanduser("~/.ssh/config"))
+        try:
+            self.target = config.host(target)['hostname']
+        except KeyError:
+            self.target = target
         self.count = count
         self.verbose = verbose
         self.timeout = timeout
@@ -32,15 +30,15 @@ class SSHPing():
         except PermissionError:
             print('Privileges required: setuid the script, or uso sudo. RAW capability required to send ping.')
 
+
 def run():
-    # argumentparsing goes here
-    parser = argparse.ArgumentParser(description='''Simple command line
-tool that lets you ping hosts that are only defined in your ssh config''')
+    parser = argparse.ArgumentParser(description='''Simple command line tool that lets you ping hosts that are only defined in your ssh config''')
     parser.add_argument('target')
     args = parser.parse_args()
     # instantiation goes here
     sshping = SSHPing(target=sys.argv[1], verbose=True)
     sshping.ping()
-    
+
+
 if __name__ == '__main__':
     run()
